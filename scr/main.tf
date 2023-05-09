29d0296c4950961e22502efff30f0d23274cc873
Oleh Kuzo <olegkuzo23@gmail.com>
2023-05-10 00:48:22 +0300

    added iam

--- scr/main.tf
+++ scr/main.tf
@@ -19,4 +19,18 @@ module "lambda" {
     table_author_arn = module.author.table_arn
     table_courses_name = module.course.table_name
     table_courses_arn = module.course.table_arn
+    lambda_courses_role_arn = module.iam.table_courses_role_arn
+
+    name_get_course = "get-course"
+    name_save_course = "save-course"
+    name_update_course = "update-course"
+    name_delete_course = "delete-course"
+}
+
+module "iam" {
+    source = "./modules/iam/eu-central-1"
+    context = module.label.context
+    name = "iam"
+    table_author_arn = module.author.table_arn
+    table_courses_arn = module.course.table_arn
 }
